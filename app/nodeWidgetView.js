c72f797149e0f23e394c00982e9a33417d30d955
Ilja Gužovski <ilja.guzovski@gmail.com>
2015-08-24 16:37:52 +0300

    more functional

--- app/nodeWidgetView.js
+++ app/nodeWidgetView.js
@@ -3,6 +3,7 @@ var NodeWidgetView = Backbone.View.extend({
     el: DOMHelper.getDefaultAnchor(),
     registry: new NodeRegistry(),
     DOM: DOMHelper,
+    that: this,
 
     initialize: function () {
         if (this.registry.hasPreviousSession()) {
@@ -14,27 +15,23 @@ var NodeWidgetView = Backbone.View.extend({
 
     render: function () {
         this.renderDOM();
+        this.DOM.hideSubNodes();
         this.setNodesHandlers();
         this.setResetButtonHandler();
-        this.DOM.hideSubNodes();
         return this;
     },
 
     renderDOM: function () {
-        var nodes = this.registry.getNodes();
-        var html = '';
-        for (var i = 0; i < nodes.length; i++) {
-            var node = nodes[i];
-            html += node.getHtml();
-        }
+        var that = this;
+        var html = _.reduce(that.registry.getNodes(), function (html, node) {
+            return html + node.getHtml();
+        }, '');
         this.el.innerHTML = this.DOM.createResetButton() + html;
     },
 
     setNodesHandlers: function () {
-        var nodes = this.DOM.getAllNodes();
-        for (var i = 0; i < nodes.length; i++) {
-            new NodeEventHandler(nodes[i], this.registry);
-        }
+        var that = this;
+        _.map(this.DOM.getAllNodes(), function(node){ return new NodeEventHandler(node, that.registry) });
     },
 
     setResetButtonHandler: function () {
