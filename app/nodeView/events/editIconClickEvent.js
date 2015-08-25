05dce5113e0a8451ea79440ef4b0ab34a924cfd1
Ilja Gužovski <ilja.guzovski@gmail.com>
2015-08-25 10:32:48 +0300

    refactor editView

--- app/nodeView/events/editIconClickEvent.js
+++ app/nodeView/events/editIconClickEvent.js
@@ -1,43 +1,43 @@
 var EditNodeView = Backbone.View.extend({
     DOM: DOMHelper,
 
-    initialize: function(options){
+    initialize: function (options) {
         this.registry = options.registry;
     },
 
     events: {
         "click .edit-current-node-save-button": "save",
-        "click .edit-current-node-cancel-button": "save",
-        "click .add-node": "addEvent"
+        "click .edit-current-node-cancel-button": "cancel"
     },
 
     render: function () {
         if (this.DOM.nodeEditFormDoesNotExist(this.el)) {
-            this.createEditForm(this.el);
+            this.createEditForm();
         }
+        this.editNodeForm = this.$el.find('.edit-current-node')[0];
     },
 
-    createEditForm: function(node) {
-        this.DOM.removeEditOrNewNodeForm(node);
+    createEditForm: function () {
+        this.destroyEditOrAddForm();
         var editForm = this.DOM.createEditNodeForm();
-        var firstSubNodeOfNode = this.DOM.getFirstSubNodeOfNode(node);
-        node.insertBefore(editForm, firstSubNodeOfNode);
+        var firstSubNodeOfNode = this.DOM.getFirstSubNodeOfNode(this.el);
+        this.el.insertBefore(editForm, firstSubNodeOfNode);
+    },
+
+    destroyEditOrAddForm: function () {
+        this.DOM.removeEditOrNewNodeForm(this.el);
     },
 
     save: function () {
-        var editNodeForm = this.$el.find('.edit-current-node')[ 0 ];
-        var node = this.DOM.getNodeOfNodeEditForm(editNodeForm);
-        var nodeId = this.DOM.getNodeDataId(node);
-        var newName = this.DOM.getEditedNodeNameOfNodeEditForm(editNodeForm);
-        var nodeObject = this.registry.getNodeById(nodeId);
-        nodeObject.set('name', newName);
+        var newName = this.DOM.getEditedNodeNameOfNodeEditForm(this.editNodeForm);
+        this.model.set('name', newName);
         this.registry.save();
-        this.DOM.changeNameOfNode(node, newName);
-        editNodeForm.remove();
+        this.DOM.changeNameOfNode(this.el, newName);
+        this.editNodeForm.remove();
     },
 
-    cancel: function(cancelButton) {
-        this.el.remove();
+    cancel: function () {
+        this.editNodeForm.remove();
     }
 
 });
